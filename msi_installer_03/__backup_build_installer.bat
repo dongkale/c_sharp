c9e12d83f932170f8166cb0ad3e343424df53942
LENNON\dklee <dongkale@naver.com>
2024-07-24 19:20:20 +0900

    added

--- msi_installer_03/__backup_build_installer.bat
+++ msi_installer_03/__backup_build_installer.bat
@@ -3,5 +3,5 @@ set WIX_PATH=C:\Program Files\WiX Toolset v5.0\bin
 set PROJECT_PATH=%cd%
 cd %PROJECT_PATH%\Installer
 "%WIX_PATH%\candle.exe" Product.wxs -ext WixUIExtension
-"%WIX_PATH%\light.exe" -out output\MyAppInstaller.msi Product.wixobj -cultures:ko-KR -ext WixUIExtension -spdb
+"%WIX_PATH%\light.exe" -out output\MyAppInstaller.msi Product.wixobj -cultures:ko-KR -dWixUILicenseRtf=license.rtf -ext WixUIExtension -spdb
 cd %PROJECT_PATH%
