9eb2ac1592bab73b4756075ff075a4e0cbee57a5
LENNON\dklee <dongkale@naver.com>
2024-07-29 19:06:54 +0900

    added

---
+++ file_downloader/FileDownloader4_continue_multi_complete/WindowsFormsApp4/TextProgressBar2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public class TextProgressBar2 : ProgressBar
+    {
+        public TextProgressBar2() : base()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+            DoubleBuffered = true; // remove flicker
+        }
+
+        // unhide Text/Font Properties and force changes to re-render control
+
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public override string Text
+        {
+            get => base.Text;
+            set
+            {
+                base.Text = value;
+                Refresh();
+            }
+        }
+
+        [Browsable(true)]
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public override Font Font
+        {
+            get => base.Font;
+            set
+            {
+                base.Font = value;
+                if (!string.IsNullOrWhiteSpace(Text)) Refresh();
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            base.OnPaintBackground(pevent);
+
+            // draw progress bar background
+            ProgressBarRenderer.DrawHorizontalBar(pevent.Graphics, new Rectangle(0, 0, Width, Height));
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // draw progress on progress bar
+            double percentage = ((double)(Value - Minimum)) / ((double)(Maximum - Minimum));
+            ProgressBarRenderer.DrawHorizontalChunks(e.Graphics, new Rectangle(0, 0, (int)(Width * percentage), Height));
+
+            // draw text on progress bar
+            using (Brush brush = new SolidBrush(ForeColor))
+            {
+                // get rendered size of text
+                var size = e.Graphics.MeasureString(Text, Font, new SizeF(Width, Height));
+
+                // calculate location to center text on progress bar
+                var location = new PointF((Width - size.Width) * 0.5f, (Height - size.Height) * 0.5f);
+
+                // draw text
+                e.Graphics.DrawString(Text, Font, brush, new RectangleF(location, size));
+            }
+        }
+    }
+}
---
+++ winforms_src/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace winforms_src;
+
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnAddUrl;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnAddUrl = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // txtUrl
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(360, 20);
+            this.txtUrl.TabIndex = 0;
+
+            // btnAddUrl
+            this.btnAddUrl.Location = new System.Drawing.Point(378, 10);
+            this.btnAddUrl.Name = "btnAddUrl";
+            this.btnAddUrl.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUrl.TabIndex = 1;
+            this.btnAddUrl.Text = "추가";
+            this.btnAddUrl.UseVisualStyleBackColor = true;
+            this.btnAddUrl.Click += new System.EventHandler(this.btnAddUrl_Click);
+
+            // btnSelectFolder
+            this.btnSelectFolder.Location = new System.Drawing.Point(378, 38);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 2;
+            this.btnSelectFolder.Text = "폴더 선택";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+
+            // txtFolder
+            this.txtFolder.Location = new System.Drawing.Point(12, 40);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(360, 20);
+            this.txtFolder.TabIndex = 3;
+            this.txtFolder.Text = "C:\\downloadedFiles";
+
+            // btnDownload
+            this.btnDownload.Location = new System.Drawing.Point(459, 10);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "다운로드";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+
+            // flowLayoutPanel1
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 70);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(522, 300);
+            this.flowLayoutPanel1.TabIndex = 5;
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 382);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.btnAddUrl);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "멀티 파일 다운로더";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
